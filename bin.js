7f96bcc5ecb5153495f3225cc6f29672eb01e355
Georg Böte <georg@neuberger-consulting.com>
2022-05-05 13:01:29 +0200

    Add yargs package

--- bin.js
+++ bin.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log('bin hello!');
+
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+
+const workingdir = process.cwd();
+const localconfig = path.join(workingdir, 'slingshot.config.js');
+const args = yargs.argv;
+
+console.log(workingdir);
+console.log(localconfig);
+
+console.log(fs.existsSync(localconfig));
+console.log(args);
