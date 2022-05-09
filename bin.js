bb3f1931cfc004121f99fee3427a9ad445380db9
Georg Böte <georg@neuberger-consulting.com>
2022-05-09 11:32:30 +0200

    latest changes (broken)

--- bin.js
+++ bin.js
@@ -1,36 +1,86 @@
 #!/usr/bin/env node
+// node modules
 const fs = require('fs');
 const path = require('path');
-const yargs = require('yargs');
-const args = yargs.argv;
 
-// default configs
-let config = { mode: 'ssh' };
+// custom modules
+const colors = require('./src/cli-colors');
+
+// setup sling instance
+const sling = new (require('./index'))().set('cli', true);
 
 // the working directory
 const workingdir = process.cwd();
 
-// default path to config is in current working directory
-let configpath = path.join(workingdir, 'slingshot.config.js');
+// default config filename
+const configfilename = 'slingshot.config.js';
 
-// use --config param if valid
-if (args.config) {
-  configpath = args.config;
+// paths to search for the config file
+const pathsToLookForConfig = [
+  path.join(workingdir, './config/', configfilename),
+  path.join(workingdir, configfilename),
+];
 
-  // assume local path when path does not
-  // start with forward slash.
-  if (!configpath.startsWith('/')) {
-    configpath = path.join(workingdir, args.config);
+// valid path after lookup
+let configpath = null;
+
+// search for config file
+pathsToLookForConfig.forEach((p) => {
+  if (!configpath && fs.existsSync(p)) {
+    configpath = require(p);
   }
+});
+
+// default configs
+let config = require('./config.defaults');
+const logger = require('./src/logger');
+
+// merge defaults with found config
+if (configpath) {
+  config = { ...config, ...configpath };
 }
 
-// Merge given config with default one
-if (fs.existsSync(configpath)) {
-  config = Object.assign({}, config, require(configpath));
-}
+// set initial config for sling
+sling.set('config', config);
 
-// create slingshot instance
-const sling = new (require('./index'))(config);
+// setup cli yargs parser
+require('yargs/yargs')(process.argv.slice(2))
+  .scriptName('slingshot')
+  .usage('$0 <command> [args...]')
+  .option('config', {
+    alias: 'c',
+    describe: 'Path to config file.',
+    requiresArg: true,
+  })
+  .coerce('config', function (arg) {
+    configpath = arg.startsWith('/') ? path.join(arg) : path.join(workingdir, arg);
 
-// execute slingshot
-sling.exec();
+    if (!fs.existsSync(configpath)) {
+      throw new Error(`Config file not found: ${configpath}`);
+    }
+
+    const loadedconfig = require(configpath);
+    sling.set('config', { ...config, ...loadedconfig });
+  })
+  .command('init', 'Create slingshot config file.')
+  .command(
+    'deploy [revision]',
+    'Deploy specific revision. Could be any git tag, commit, branch. Defaults to "master".',
+    () => {},
+    (arg) => sling.deploy(arg.revision)
+  )
+  .command(
+    'rollback',
+    'Roll back to previously published release',
+    () => {},
+    function () {
+      console.log('rollback');
+    }
+  )
+  .demandCommand(1, 'You need to provide at least one command to execute.')
+  .fail((msg, err, yargs) => {
+    console.log(`\n${colors.fgRed}Error: ${colors.reset}${msg}\n`);
+    console.log(yargs.help());
+    process.exit(1);
+  })
+  .help().argv;
--- index.js
+++ index.js
@@ -1,37 +1,177 @@
+/** global process */
+const fs = require('fs');
+const path = require('path');
+const { NodeSSH } = require('node-ssh');
+const colors = require('./src/cli-colors');
+const logger = require('./src/logger');
+const { resolveHomePath } = require('./src/helper');
+
+const keyFileNames = ['id_rsa', 'id_ed25519'];
+
 class Slingshot {
-  constructor(opts = {}) {
-    this.config = Object.assign({}, opts);
+  constructor(config = {}, cli = null) {
+    this.config = { ...config };
+    this.cli = cli;
+    this.ssh = new NodeSSH();
     return this;
   }
 
   set(key, val = null) {
-    this.config[key] = val;
+    switch (key) {
+      case 'cli':
+        this.cli = val;
+        break;
+
+      case 'config':
+        this.config = { ...this.config, ...val };
+        break;
+
+      case null:
+        break;
+
+      default:
+        this.config[key] = val;
+        break;
+    }
+
     return this;
   }
 
-  _checkConfig() {
-    if (!this.config.mode) {
-      throw `No transport mode set in config!`;
+  async _checkConfig() {
+    if (!this.config.username || !this.config.host) {
+      throw `Credentials are missing!`;
     }
 
-    if (!this.config.user || !this.config.host) {
-      throw `Credentials are invalid!`;
+    this.sshconfig = {
+      host: this.config.host,
+      port: this.config.port,
+      username: this.config.username,
+    };
+
+    if (this.config.privateKey) {
+      this.sshconfig.privateKey = this._resolveSSHKey(this.config.privateKey);
     }
+
+    if (this.config.password) {
+      this.sshconfig.password = this.config.password;
+    }
+
+    // try to extract from local key files if nothing has been passed as option
+    if (this._noSSHCredentialsSet()) {
+      for (var i = 0; i < keyFileNames.length; i++) {
+        const privateKey = path.join(process.env.HOME, '.ssh/', keyFileNames[i]);
+
+        if (fs.existsSync(privateKey)) {
+          this.sshconfig.privateKey = this._resolveSSHKey(privateKey);
+          break;
+        }
+      }
+    }
+
+    // throw error if still no ssh credentials are set
+    if (this._noSSHCredentialsSet()) {
+      throw `SSH Credentials are missing!`;
+    }
+
+    logger.stepsuccess(`valid config`);
+
+    return true;
   }
 
-  _checkConnection() {}
-
-  _deploy() {
-    console.log('run deploy!');
+  _noSSHCredentialsSet() {
+    return !this.sshconfig.password && !this.sshconfig.privateKey;
   }
 
-  async exec() {
+  _resolveSSHKey(filepath) {
+    const p = resolveHomePath(filepath);
+    return fs.existsSync(p) ? p : null;
+  }
+
+  _checkConnection() {
+    return new Promise((resolve, reject) => {
+      this.ssh
+        .connect(this.sshconfig)
+        .then(() => {
+          logger.stepsuccess('connection established');
+          resolve();
+        })
+        .catch((e) => {
+          logger.steperror('connection not established');
+          console.error(e);
+          reject(e);
+        });
+    });
+  }
+
+  async _checkRemoteStructure() {
+    logger.task(`check remote structure`);
+
+    this.ssh
+      .connect(this.sshconfig)
+      .then(() => {})
+      .catch((e) => {});
+
+    return true;
+  }
+
+  async _deploy() {
     try {
-      this._checkConfig();
-      this._checkConnection();
-      this._deploy();
+      await this._checkRemoteStructure();
+      await this._releaseCreate();
+
+      Promise.resolve();
     } catch (error) {
-      console.error(error);
+      console.error(`\n${error}`);
+      Promise.reject();
+      process.exit(1);
+    }
+  }
+
+  async deploy(arg = null) {
+    // assume argument is a git revision
+    // and update config field related to it.
+    if (arg) {
+      this.config.revision = arg;
+    }
+
+    try {
+      // start output
+      logger.task(`checks`);
+
+      // first check the config file
+      await this._checkConfig();
+
+      // test the connection
+      await this._checkConnection();
+
+      // test remote folder structure
+      await this._checkRemoteStructure();
+
+      // success 'check' task
+      logger.success();
+
+      // run beforedeploy hook
+      if (typeof this.config.beforeDeploy === 'function') {
+        logger.task(`hook beforeDeploy()`);
+        await this.config.beforeDeploy(this);
+        logger.success();
+      }
+
+      // actually run the upload
+      await this._deploy();
+
+      // run afterdeploy hook
+      if (typeof this.config.beforeDeploy === 'function') {
+        logger.task(`hook afterDeploy()`);
+        await this.config.afterDeploy(this);
+        logger.success();
+      }
+
+      Promise.resolve();
+      process.exit(0);
+    } catch (error) {
+      console.error(`\n${error}`);
+      Promise.reject();
       process.exit(1);
     }
   }
